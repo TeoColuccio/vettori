448a2c1d1bb70df7cee1f0c9b82522efeb2b86d8
Teo <teo@teo-HP-Pavilion.station>
2017-02-04 18:28:07 +0100

    Fix Cmake

---
+++ dado/build/CMakeFiles/LanciaDado.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LanciaDado.dir/main.c.o"
+  "LanciaDado.pdb"
+  "LanciaDado"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LanciaDado.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
