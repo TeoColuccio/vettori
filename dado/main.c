4083df2a7b27117143d8762065705606311acfe6
Teodoro777 <teodoro.coluccio@gmail.com>
2017-02-10 16:05:01 +0100

    Ora il main lancia due dadi

--- dado/main.c
+++ dado/main.c
@@ -11,20 +11,33 @@
 int main() 
 {
   int i, num, lanci;
-  int occorrenze[6] = { 0 };
+  int dado_x[6] = { 0 };
+  int dado_y[6] = { 0 };
 
   dado_init();
 
   for (lanci = 0;  lanci < 30000; lanci++) {
 
     num = dado_extract(6);
-    occorrenze[num - 1]++;
+    dado_x[num - 1]++;
+  }
+  
+  for (lanci = 0;  lanci < 30000; lanci++) {
+
+    num = dado_extract(6);
+    dado_y[num - 1]++;
   }
  
-  printf("Ho lanciato il dato 30000 volte...\n");
-
+  printf("Ho lanciato il dado 30000 volte...\n");
+  printf("Ho lanciato il secondo dado 30000 volte...\n");
+  
+  printf("***Primo dado***\n");
   for (i = 0; i < 6; i++) {
-    printf("Il numero %d e' stato estratto %d volte\n", i+1, occorrenze[i]);
+    printf("Il numero %d e' stato estratto %d volte\n", i+1, dado_x[i]);
+  }
+  printf("***Secondo dado***\n");
+  for (i = 0; i < 6; i++) {
+    printf("Il numero %d e' stato estratto %d volte\n", i+1, dado_y[i]);
   }
 
   return 0;
