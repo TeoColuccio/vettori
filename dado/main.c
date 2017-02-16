adce73aa268a02ee742aeb6939cc9797d9150cae
Teodoro777 <teodoro.coluccio@gmail.com>
2017-02-16 21:54:03 +0100

    Creato un vettore per la percentuale

--- dado/main.c
+++ dado/main.c
@@ -12,7 +12,7 @@ int main()
 {
   int i = 7, x, y, lanci;
   int somma[13] = { 0 };
-  float percentuale;
+  float percentuale [13] = { 0., 0., 1./36., 2./36., 3./36., 4./36., 5./36., 6./36., 5./36., 4./36., 3./36., 2./36., 1./36.};
   
   dado_init();
 
@@ -22,23 +22,14 @@ int main()
     y = dado_extract(6);
 
     somma[x + y]++; 
-    
-  }
+    }
   
-  printf("Ho lanciato due dadi 30000 volte...\n");
-  
-  for (percentuale = 2; percentuale < 7; percentuale++) {
-    printf("La percentuale teorica che esca %.0f e' %.2f\n", percentuale, (percentuale-1)*100 / 36); 
-  }
-  for (percentuale = 6; percentuale > 0; percentuale--){
-      printf("La percentuale teorica che esca %d e' %.2f\n", i, (percentuale*100) / 36); 
-    i++;
-  }
+  printf("Ho lanciato due dadi 30000 volte...\n");  
   
   for (i = 2; i < 13; i++) {
-    printf("Il numero %d e' stato estratto %d volte, con percentuale reale %.2f\n", i, somma[i], (somma[i] * 100.0) / 30000.0);
+    printf("Il numero %d e' stato estratto %d volte, con percentuale reale %.2f...\n", i, somma[i], (somma[i] * 100.0) / 30000.0);
+    printf("C'era una percentuale teorica pari a %.2f.\n", percentuale[i]*100);
   }
 
-  
   return 0;
 }
