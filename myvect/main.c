fddf235fe588e444d73e02915367480557e35706
Teodoro777 <teodoro.coluccio@gmail.com>
2017-02-27 19:17:01 +0100

    Modifica CMake

--- myvect/main.c
+++ myvect/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "myvect.h"
+#include "myvect_math.h"
 
 int main() {
   int size = 7;
@@ -10,7 +11,7 @@ int main() {
   myvect_init_in_range(vett, size, 2, 10);  
   myvect_print(vett, size);
   
-  printf("La somma degli elementi e': %.2f, e la media: %.2f", myvect_math_sum(vett, size), myvect_math_mean(vett, size));
+  printf("La somma degli elementi e': %d, e la media: %f", myvect_math_sum(vett, size), myvect_math_mean(vett, size));
 
   myvect_print(vett, size);
   
