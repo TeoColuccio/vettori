a699fdd843270652d66dcdff239115cc2924db5d
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-02-27 21:03:59 +0100

    Update myvect_math.c

--- myvect/myvect_math/myvect_math.c
+++ myvect/myvect_math/myvect_math.c
@@ -9,6 +9,7 @@ int myvect_math_sum(int vett[], int size)
   return sum;
 }
 
+/* in questa funzione c'e' un errore */
 float myvect_math_mean(int vett[], int size) 
 {
   return myvect_math_sum(vett[], size) / size;
