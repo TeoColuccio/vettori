570abd126afc7b95f2ef3e825a7ee27c56f6a1e1
Teodoro777 <teodoro.coluccio@gmail.com>
2017-04-08 18:44:18 +0200

    Caricato secondo miglioramento

---
+++ bubble_sort/bubble_sort_b.c
@@ -0,0 +1,56 @@
+/* bubble_sort.c */
+
+/* Illustra il metodo di ordinamento a bolle */
+
+#include <stdio.h>
+#include "myvect.h"
+
+#define SIZE 10
+
+void bubble_sort(int v[], const int size);
+
+int main()
+{
+  int v[SIZE];
+
+  myvect_init();
+
+  myvect_init_in_range(v, SIZE, -20, 100);
+
+  printf("Vettore prima dell'ordinamento:\n");
+  myvect_print(v, SIZE);
+
+  bubble_sort(v, SIZE);
+
+  printf("Vettore dopo l'ordinamento:\n");
+  myvect_print(v, SIZE);
+
+  return 0;
+}
+
+void bubble_sort(int v[], const int size)
+{
+  int pass;
+  int i;
+  int temp, scambi = 0;
+
+  for (pass=1; pass<size; pass++) {
+    /* per ogni passaggio fatto sul vettore
+     * controlla se l'elemento i-esimo e'
+     * maggiore dell'elemento (i+1)-esimo
+     * e in tal caso li scambia
+     */
+      break;
+    for (i=pass-1; i<size-1; i++) {
+      if (v[i] > v[i+1]) {
+        temp = v[i];
+        v[i] = v[i+1];
+        v[i+1] = temp;
+        scambio++;
+      
+      if (scambi == 0) 
+        break;
+      }
+    }
+  }
+}
