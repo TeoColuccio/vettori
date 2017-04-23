1744222e0b63c4e533abfd798812d1c106245e1e
Teodoro777 <teodoro.coluccio@gmail.com>
2017-04-23 11:07:59 +0200

    Primo passo

---
+++ selection_sort/random/random.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include "random.h"
+
+void random_init()
+{
+  srand(time(NULL));
+}
+
+int random_between(int min, int max)
+{
+  return min + rand()% (max - min);
+}
---
+++ selection_sort/selection_sort.c
@@ -0,0 +1,46 @@
+/* Il programma ordinera' gli elementi interi di
+ * un vettore con una funzione selection_sort() 
+ */
+
+
+#include <stdio.h>
+#include "myvect.h"
+
+#define SIZE 4
+
+void selection_sort(int v[], const int size, int lim);
+
+int main()
+{
+  int v[SIZE];
+
+  myvect_init();
+
+  myvect_init_in_range(v, SIZE, 0, 100);
+
+  printf("Vettore prima dell'ordinamento:\n");
+  myvect_print(v, SIZE);
+
+  selection_sort(v, SIZE, 100);
+
+  printf("Vettore dopo l'ordinamento:\n");
+  myvect_print(v, SIZE);
+
+  return 0;
+}
+
+void selection_sort(int v[], const int size, int lim)
+{
+  int i;
+  int temp;
+
+  for (i=0; i<size; i++) {
+    if (v[i] < lim) { 
+      lim = v[i];
+      
+      temp = v[0];
+      v[0] = v[i];
+      v[i] = temp;
+    }
+  }
+}
