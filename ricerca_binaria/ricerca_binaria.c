abc0c3e6bd8c7d79a84b1ac1952efd6344124029
Teodoro777 <teodoro.coluccio@gmail.com>
2017-06-01 20:16:48 +0200

    Fix confusione tra indice ed elemento

--- ricerca_binaria/ricerca_binaria.c
+++ ricerca_binaria/ricerca_binaria.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include "myvect.h"
 
-#define SIZE 8 
+#define SIZE 30 
 
 void bubble_sort(int v[], const int size);
 int ricerca_binaria(int x, int v[], int low, int high);
@@ -15,16 +15,16 @@ int ricerca_binaria(int x, int v[], int low, int high);
 int main()
 {
   int v[SIZE];
-  int x = 3, ris;
+  int x = 87, ris;
 
   myvect_init();
-  myvect_init_in_range(v, SIZE, 0, 10);
+  myvect_init_in_range(v, SIZE, 0, 100);
   bubble_sort(v, SIZE);
   myvect_print(v, SIZE);
   
   ris = ricerca_binaria(x, v, 0, SIZE);
   if (ris != -1) printf("L'elemento %d si trova all'indice %d del vettore.\n", x, ris);
-  else printf("L'elemento non e' stato trovato.\n"); 
+  else printf("L'elemento %d non e' stato trovato.\n", x); 
   
   return 0;
 }
@@ -36,11 +36,6 @@ void bubble_sort(int v[], const int size)
   int temp, scambi = 1;
 
   for (pass=1; pass<size && scambi; pass++) {
-    /* per ogni passaggio fatto sul vettore
-     * controlla se l'elemento i-esimo e'
-     * maggiore dell'elemento (i+1)-esimo
-     * e in tal caso li scambia
-     */
     scambi = 0;
     for (i=0; i<size-pass; i++) {
       if (v[i] > v[i+1]) {
@@ -57,9 +52,11 @@ int ricerca_binaria(int x, int v[], int low, int high)
 {
   int i, med;
   
-  med = (low+high) / 2;
+  printf("Low: %d, High: %d\n", low, high);
+  if (low >= high) return -1;
+
+  med = (low+high)/ 2;
   if (x == v[med]) return med;
-  else if (x < med) ricerca_binaria (x, v, 0, med);
-  else if (x > med) ricerca_binaria (x, v, med, high);
-  else if (x != v[med]) return -1;
+  else if (x < v[med]) ricerca_binaria (x, v, low, med-1);
+  else if (x > v[med]) ricerca_binaria (x, v, med+1, high);
 }
