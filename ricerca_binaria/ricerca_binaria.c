01a436a4ec70c29a6a7752de25f4de85c88e5059
Teodoro777 <teodoro.coluccio@gmail.com>
2017-06-05 18:00:38 +0200

    Creata funzione helper

--- ricerca_binaria/ricerca_binaria.c
+++ ricerca_binaria/ricerca_binaria.c
@@ -10,18 +10,19 @@
 #define SIZE 5 
 
 void bubble_sort(int v[], const int size);
-int ricerca_binaria(int x, int v[], int low, int high);
+int ricerca_binaria(int x, int v[], int size);
+int ricerca_binaria_helper(int x, int v[], int low, int high);
 
 int main()
 {
   int v[SIZE] = {7, 9, 10, 77, 87};
-  int x = 77, ris;
+  int x = 7, ris;
 
   myvect_init();
   bubble_sort(v, SIZE);
   myvect_print(v, SIZE);
   
-  ris = ricerca_binaria(x, v, 0, SIZE);
+  ris = ricerca_binaria(x, v, SIZE);
   if (ris != -1) printf("L'elemento %d si trova all'indice %d del vettore.\n", x, ris);
   else printf("L'elemento %d non e' stato trovato.\n", x); 
   
@@ -47,7 +48,12 @@ void bubble_sort(int v[], const int size)
   }
 }
 
-int ricerca_binaria(int x, int v[], int low, int high)
+int ricerca_binaria(int x, int v[], int size)
+{
+  return ricerca_binaria_helper(x, v, 0, size);
+}
+
+int ricerca_binaria_helper(int x, int v[], int low, int high)
 {
   int i, med;
   
@@ -56,6 +62,6 @@ int ricerca_binaria(int x, int v[], int low, int high)
 
   med = (low+high)/ 2;
   if (x == v[med]) return med;
-  else if (x < v[med]) ricerca_binaria (x, v, low, med-1);
-  else if (x > v[med]) ricerca_binaria (x, v, med+1, high);
+  else if (x < v[med]) ricerca_binaria_helper(x, v, low, med-1);
+  else if (x > v[med]) ricerca_binaria_helper(x, v, med+1, high);
 }
