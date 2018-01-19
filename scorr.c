5d7a75acc4be7e7cbab5f948e5cf9a15245f5c24
Teodoro777 <teodoro.coluccio@gmail.com>
2018-01-19 22:28:19 +0100

    Commento del codice

--- scorr.c
+++ scorr.c
@@ -3,7 +3,7 @@
 #define DIM  10
 
 int vett1[DIM] = {1, 2, 4, 6, 7, 8, 8, 8, 8, 3};
-int vett2[DIM] = {2, 4, 6, 7, 8, 8, 8, 8, 3, 1};
+int vett2[DIM] = {4, 6, 7, 8, 8, 8, 8, 3, 1, 2};
 int vett3[DIM] = {1, 2, 4, 6, 7, 8, 8, 8, 8, 3};
 
 void scorr();
@@ -11,18 +11,18 @@ void scorr();
 int main() {
   int i;
   
-  for(i=0;i <10; i++) {
+  for(i=0;i <10; i++) { // stampa vett1 iniziale
     printf("%d", vett1[i]);
   }
 
   printf("\n");
 
-  for(i=0;i <10; i++) {
+  for(i=0;i <10; i++) { // stampa vett2 iniziale
     printf("%d", vett2[i]);
   }
   printf("\n");
 
-  scorr();
+  scorr(); // chiamo la funzione
   
   return 0;
 }
@@ -30,28 +30,29 @@ int main() {
 void scorr() {
   int count = 0, i = 0, temp, c = 0, t = 0;
 
-  while (t < 10) {
-    if (vett1[c] != vett2[c]) {
-      temp = vett2[DIM - 1];
+  while (t < 10) { // potranno essere compiuti al massimo 10 scorrimenti
+    while (vett1[c] != vett2[c]) { //controllo gli indici iniziali
+      temp = vett2[DIM - 1]; // inizio lo scorrimento circolare
       while (i < DIM) {
         vett2[DIM - i - 1] = vett2[DIM - i - 2]; 
         i++;
       }
       vett2[0] = temp;
-      count++;
+      count++; // aggiorno gli scorrimenti
+      c++; // controllo l'indice successivo
+      i++; // aggiorno "i" per il prossimo scorrimento 
     }
-    c++;
-    t++;
+    t++; // aggiorno la variabile per il while iniziale
   }
 
-  for(i=0;i <10; i++) {
+  for(i=0;i <10; i++) { // stampo vett1 finale
     printf("%d", vett1[i]);
-  }
+  } 
   
   printf("\n");
 
-  for(i=0;i <10; i++) {
+  for(i=0;i <10; i++) { //stampo vett2 finale
     printf("%d", vett2[i]);
-  }
-  printf("\n%d\n",count);
+  } 
+  printf("\nScorrimenti: %d\n",count); // stampo il numero degli scorrimenti
 }
